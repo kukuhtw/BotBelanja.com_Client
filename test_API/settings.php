5db8b1f40959bf32b0dcda2ec29e4c73078ca6f8
Kukuh TW <kukuhtw@gmail.com>
2022-10-12 10:31:18 +0700

    Update settings.php
    https

--- test_API/settings.php
+++ test_API/settings.php
@@ -3,6 +3,6 @@ $apps_id = ""; // <-- PUT APPS ID HERE
 $owner_id = ""; // <-- PUT OWNER ID HERE 
 $Header_ClientID="";  // <-- PUT Header_ClientID  HERE 
 $Header_PassKey=""; // <-- PUT Header_PassKey  HERE 
-$BASE_END_POINT_SAAS="http://botbelanja.com/API/"; // <-- PUT BASE_END_POINT_SAAS BotBelanja.com HERE 
+$BASE_END_POINT_SAAS="https://botbelanja.com/API/"; // <-- PUT BASE_END_POINT_SAAS BotBelanja.com HERE 
 
 ?>
