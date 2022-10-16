10a01894bdf83f6e14e631affe174d363b4499c7
Kukuh TW <kukuhtw@gmail.com>
2022-10-16 15:14:57 +0700

    Update telegram_settings.php
    telegramid info

--- webhook_telegram/telegram_settings.php
+++ webhook_telegram/telegram_settings.php
@@ -7,7 +7,18 @@ $URL="https://api.telegram.org/bot".$API_KEY;
 $host="yourdomain.com/yourfolder"; //your domain and your folder
 $URL_BASE = "https://".$host."/webhook_telegram/".$filehookname;
 
+
 $TELEGRAM_ADMIN =""; // your id telegram
+/* 
+to know your telegram id, check this
+https://t.me/userinfobot, type anything
 
+you will get info like this
+@[YOUR_TELEGRAM_USERNAME]
+Id: [YOUR_TELEGRAM_ID]
+First: [YOUR FIRST NAME]
+Last: [YOUR LAST NAME]
+Lang: en
 
+*/
 ?>
